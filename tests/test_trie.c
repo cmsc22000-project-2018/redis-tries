7cae27a2b482aa0af231b3fd05da506fc714036f
Adela Deng <33789376+dengyijia@users.noreply.github.com>
2018-05-06 18:23:18 -0500

    Add files via upload

---
+++ tests/test_trie.c
@@ -0,0 +1,45 @@
+#include <criterion/criterion.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "trie.h"
+
+Test(trie, new)
+{
+    trie_t *t;
+
+    t = new_trie('c');
+
+    cr_assert_not_null(t, "new_trie() failed to allocate memory");
+    cr_assert_eq(t->current, 'c', "new_trie() failed to set current");
+    cr_assert_eq(t->is_word, 0, "new_trie() failed to set is_word");
+}
+
+Test(trie, add_node_exists)
+{
+
+}
+
+Test(trie, add_node_new)
+{
+
+}
+
+Test(trie, insert_string)
+{
+    
+}
+
+Test(trie, free)
+{
+    trie_t *t;
+    int rc;
+
+    t = new_trie('\0');
+
+    cr_assert_not_null(t, "new_trie() failed to allocate memory");
+
+    rc = trie_free(t);
+
+    cr_assert_eq(rc, SUCCESS, "trie_free failed");
+
+}
