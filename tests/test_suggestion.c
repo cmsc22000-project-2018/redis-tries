070f4949bd20565e2cd31893623eb7d5c0960f14
Dustin Zhang <dustinzhang@cs.uchicago.edu>
2018-05-26 21:04:18 -0500

    Changed test_suggestion.c

--- tests/test_suggestion.c
+++ tests/test_suggestion.c
@@ -5,18 +5,18 @@
 
 // Test has_children for success
 Test(suggestion, has_children_s0) {
-    dict_t *d = dict_new();
-    dict_add(d, "abc");
+    trie_t *t = trie_new('\0');
+    trie_insert_string(t, "abc");
 
-    cr_assert_eq(has_children(d, "ab"), EXIT_SUCCESS, "has_children failed");
+    cr_assert_eq(has_children(t, "ab"), EXIT_SUCCESS, "has_children failed");
 }
 
 // Test has_children for failure
 Test(suggestion, has_children_f0) {
-    dict_t *d = dict_new();
-    dict_add(d, "abc");
+    trie_t *t = trie_new('\0');
+    trie_insert_string(t, "abc");
 
-    cr_assert_eq(has_children(d, "ac"), EXIT_FAILURE, "has_children failed");
+    cr_assert_eq(has_children(t, "ac"), EXIT_FAILURE, "has_children failed");
 }
 
 /*************   These functions individually test values for cmp_match   *************/
@@ -32,6 +32,8 @@ void test_cmp_match(char *m1_str, char *m2_str, int m1_val, int m2_val, int expe
     m2->edits_left = 1;
 
     int rc = cmp_match((void*)m1, (void*)m2);
+
+    printf("%d\n", rc);
 
     cr_assert_eq(rc, expected, "cmp_match on %s, %d and %s, %d returned %d instead of %d",
                  m1_str, m1_val, m2_str, m2_val, rc, expected);
@@ -50,7 +52,7 @@ Test(suggestion, cmp_match_2) {
     test_cmp_match("aa", "aa", 1, 0, -1);
 }
 
-Test(suggestion, cmp_match_1) {
+Test(suggestion, cmp_match_3) {
     test_cmp_match("aa", "aa", 0, 1, 1);
 }
 
@@ -63,10 +65,10 @@ Test(suggestion, cmp_match_null0) {
 
     int rc = cmp_match((void*)m1, (void*)NULL);
 
-    cr_assert_eq(rc, 0)
+    cr_assert_eq(rc, 1, "cmp_match with a null argument failed");
 }
 
-Test(suggestion, cmp_match_null0) {
+Test(suggestion, cmp_match_null1) {
     match_t *m1 = malloc(sizeof(match_t));
 
     m1->str = "hi";
@@ -74,317 +76,300 @@ Test(suggestion, cmp_match_null0) {
     m1->edits_left = 0;
 
     int rc = cmp_match((void*)NULL, (void*)m1);
+
+    cr_assert_eq(rc, -1, "cmp_match with a null argument failed");
 }
 
 /*************   These functions individually test helpers of suggestions()   *************/
 
 // Test a simple move_on
 Test(suggestion, move_s0) {
-    zset_t *set = zset_new("set");
-    dict_t *d = dict_new();
+    trie_t *t = trie_new('\0');
+    match_t **set = calloc(1, sizeof(match_t*));
 
-    dict_add(d, "f");
+    trie_insert_string(t, "f");
 
-    int rc = suggestions(set, d, "", "f", 1);
+    int rc = suggestions(set, t, "", "f", 1, 1);
 
     cr_assert_eq(0, rc, "suggestions() failed");
-    cr_assert_eq(zset_rank(set, "f"), 0, "move_on failed");
-
-    // cleanup
-    zset_remrangebyrank(set, 0, -1);
+    cr_assert_eq(strcmp(set[0]->str, "f"), 0, "move_on string failed");
+    cr_assert_eq(set[0]->edits_left, 1, "move_on failed");
 }
 
 // Test a full move_on
 Test(suggestion, move_s1) {
-    zset_t *set = zset_new("set");
-    dict_t *d = dict_new();
+    trie_t *t = trie_new('\0');
+    match_t **set = calloc(5, sizeof(match_t*));
 
-    dict_add(d, "vdf894jkls");
+    trie_insert_string(t, "vdf894jkls");
 
-    int rc = suggestions(set, d, "", "vdf894jkls", 1);
+    int rc = suggestions(set, t, "", "vdf894jkls", 1, 5);
 
     cr_assert_eq(0, rc, "suggestions() failed");
-    cr_assert_eq(zset_rank(set, "vdf894jkls"), 0, "move_on failed");
-
-    // cleanup
-    zset_remrangebyrank(set, 0, -1);
+    cr_assert_eq(strcmp(set[0]->str, "vdf894jkls"), 0, "move_on string failed");
+    cr_assert_eq(set[0]->edits_left, 1, "move_on value failed");
 }
 
 // Test a simple delete
 Test(suggestion, delete_s0) {
-    zset_t *set = zset_new("set");
-    dict_t *d = dict_new();
+    trie_t *t = trie_new('\0');
+    match_t **set = calloc(1, sizeof(match_t*));
 
-    int rc = suggestions(set, d, "", "k", 1);
+    int rc = suggestions(set, t, "", "k", 1, 1);
 
     cr_assert_eq(0, rc, "suggestions() failed");
-    cr_assert_eq(zset_rank(set, "cds8Dfk"), 0, "try_delete failed");
-
-    // cleanup
-    zset_remrangebyrank(set, 0, -1);
+    cr_assert_eq(strcmp(set[0]->str, "k"), 0, "try_delete string failed");
+    cr_assert_eq(set[0]->edits_left, 0, "try_delete value failed");
 }
 
 // Test a double delete
 Test(suggestion, delete_s1) {
-    zset_t *set = zset_new("set");
-    dict_t *d = dict_new();
+    trie_t *t = trie_new('\0');
+    match_t **set = calloc(5, sizeof(match_t*));
 
-    dict_add(d, "cds8Dfk");
+    trie_insert_string(t, "cds8Dfk");
 
-    int rc = suggestions(set, d, "", "cdisc8Dfk", 2);
+    int rc = suggestions(set, t, "", "cdisc8Dfk", 2, 5);
 
     cr_assert_eq(0, rc, "suggestions() failed");
-    cr_assert_eq(zset_rank(set, "cds8Dfk"), 0, "try_delete failed");
-
-    // cleanup
-    zset_remrangebyrank(set, 0, -1);
+    cr_assert_eq(strcmp(set[0]->str, "cds8Dfk"), 0, "try_delete string failed");
+    cr_assert_eq(set[0]->edits_left, 0, "try_delete value failed");
 }
 
 // Test a simple replace
 Test(suggestion, replace_s0) {
-    zset_t *set = zset_new("set");
-    dict_t *d = dict_new();
+    trie_t *t = trie_new('\0');
+    match_t **set = calloc(1, sizeof(match_t*));
 
-    dict_add(d, "s");
+    trie_insert_string(t, "s");
 
-    int rc = suggestions(set, d, "", "i", 1);
+    int rc = suggestions(set, t, "", "i", 1, 1);
 
     cr_assert_eq(0, rc, "suggestions() failed");
-    cr_assert_eq(zset_rank(set, "s"), 0, "try_replace failed");
-
-    // cleanup
-    zset_remrangebyrank(set, 0, -1);
+    cr_assert_eq(strcmp(set[0]->str, "s"), 0, "try_replace string failed");
+    cr_assert_eq(set[0]->edits_left, 0, "try_replace value failed");
 }
 
 // Test a double replace
 Test(suggestion, replace_s1) {
-    zset_t *set = zset_new("set");
-    dict_t *d = dict_new();
+    trie_t *t = trie_new('\0');
+    match_t **set = calloc(5, sizeof(match_t*));
 
-    dict_add(d, "vT.k=(?");
+    trie_insert_string(t, "vT.k=(?");
 
-    int rc = suggestions(set, d, "", "vTik=8?", 2);
+    int rc = suggestions(set, t, "", "vTik=8?", 2, 5);
 
     cr_assert_eq(0, rc, "suggestions() failed");
-    cr_assert_eq(zset_rank(set, "vjikd8-"), 0, "try_replace failed");
-
-    // cleanup
-    zset_remrangebyrank(set, 0, -1);
+    cr_assert_eq(strcmp(set[0]->str, "vT.k=(?"), 0, "try_replace string failed");
+    cr_assert_eq(set[0]->edits_left, 0, "try_replace value failed");
 }
 
 // Test a simple swap
 Test(suggestion, swap_s0) {
-    zset_t *set = zset_new("set");
-    dict_t *d = dict_new();
+    trie_t *t = trie_new('\0');
+    match_t **set = calloc(1, sizeof(match_t*));
 
-    dict_add(d, "ap");
+    trie_insert_string(t, "ap");
 
-    int rc = suggestions(set, d, "p", "a", 1);
+    int rc = suggestions(set, t, "p", "a", 1, 1);
 
     cr_assert_eq(0, rc, "suggestions() failed");
-    cr_assert_eq(zset_rank(set, "ap"), 0, "try_swap failed");
-
-    // cleanup
-    zset_remrangebyrank(set, 0, -1);
+    cr_assert_eq(strcmp(set[0]->str, "ap"), 0, "try_swap string failed");
+    cr_assert_eq(set[0]->edits_left, 0, "try_swap value failed");
 }
 
 // Test a double swap 
 Test(suggestion, swap_s1) {
-    zset_t *set = zset_new("set");
-    dict_t *d = dict_new();
+    trie_t *t = trie_new('\0');
+    match_t **set = calloc(5, sizeof(match_t*));
 
-    dict_add(d, "4uiwifnw");
+    trie_insert_string(t, "4uiwifnw");
 
-    int rc = suggestions(set, d, "", "4uiwnfiw", 2);
+    int rc = suggestions(set, t, "", "4uiwnfiw", 2, 5);
 
     cr_assert_eq(0, rc, "suggestions() failed");
-    cr_assert_eq(zset_rank(set, "4uiwifnw"), 0, "try_swap failed");
-
-    // cleanup
-    zset_remrangebyrank(set, 0, -1);
+    cr_assert_eq(strcmp(set[0]->str, "4uiwifnw"), 0, "try_swap string failed");
+    cr_assert_eq(set[0]->edits_left, 0, "try_swap value failed");
 }
 
 // Test a simple insert
 Test(suggestion, insert_s0) {
-    zset_t *set = zset_new("set");
-    dict_t *d = dict_new();
+    trie_t *t = trie_new('\0');
+    match_t **set = calloc(1, sizeof(match_t*));
 
-    dict_add(d, "+");
+    trie_insert_string(t, "+");
 
-    int rc = suggestions(set, d, "", "", 1);
+    int rc = suggestions(set, t, "", "", 1, 1);
 
     cr_assert_eq(0, rc, "suggestions() failed");
-    cr_assert_eq(zset_rank(set, "+"), 0, "try_insert failed");
-
-    // cleanup
-    zset_remrangebyrank(set, 0, -1);
+    cr_assert_eq(strcmp(set[0]->str, "+"), 0, "try_swap string failed");
+    cr_assert_eq(set[0]->edits_left, 0, "try_swap value failed");
 }
 
 // Test a double insert
 Test(suggestion, insert_s1) {
-    zset_t *set = zset_new("set");
-    dict_t *d = dict_new();
+    trie_t *t = trie_new('\0');
+    match_t **set = calloc(5, sizeof(match_t*));
 
-    dict_add(d, "fnjk43?>.f@");
+    trie_insert_string(t, "fnjk43?>.f@");
 
-    int rc = suggestions(set, d, "", "fnk43?>.f", 2);
+    int rc = suggestions(set, t, "", "fnk43?>.f", 2, 5);
 
     cr_assert_eq(0, rc, "suggestions() failed");
-    cr_assert_eq(zset_rank(set, "fnjk43?>.f@"), 0, "try_insert failed");
-
-    // cleanup
-    zset_remrangebyrank(set, 0, -1);
+    cr_assert_eq(strcmp(set[0]->str, "fnjk43?>.f@"), 0, "try_swap string failed");
+    cr_assert_eq(set[0]->edits_left, 0, "try_swap value failed");
 }
 
 /*************   These functions test suggestions() as a whole   *************/
 
 // Test regular suggestion generation 1
 Test(suggestion, suggestions_s0) {
-    zset_t *set = zset_new("set");
-    dict_t *d = dict_new();
+    trie_t *t = trie_new('\0');
+    match_t **set = calloc(4, sizeof(match_t*));
 
-    dict_add(d, "cops");
-    dict_add(d, "8op");
-    dict_add(d, "op");
-    dict_add(d, "ocp");
+    trie_insert_string(t, "cops");
+    trie_insert_string(t, "8op");
+    trie_insert_string(t, "op");
+    trie_insert_string(t, "ocp");
 
-    int rc = suggestions(set, d, "", "cop", 1);
+    int rc = suggestions(set, t, "", "cop", 1, 4);
     cr_assert_eq(0, rc, "suggestions() failed");
 
-    char **results = zset_revrange(set, 0, 3);
-
-    cr_assert_eq(0, strncmp(results[0], "op", MAXLEN), 
+    cr_assert_eq(0, strncmp(set[0]->str, "8op", MAXLEN), 
                 "suggestions() first result incorrect");
-    cr_assert_eq(0, strncmp(results[1], "ocp", MAXLEN), 
+    cr_assert_eq(0, strncmp(set[1]->str, "cops", MAXLEN), 
                 "suggestions() second result incorrect");
-    cr_assert_eq(0, strncmp(results[2], "cops", MAXLEN), 
+    cr_assert_eq(0, strncmp(set[2]->str, "ocp", MAXLEN), 
                 "suggestions() third result incorrect");
-    cr_assert_eq(0, strncmp(results[3], "8op", MAXLEN), 
+    cr_assert_eq(0, strncmp(set[3]->str, "op", MAXLEN), 
                 "suggestions() fourth result incorrect");
-
-    zset_remrangebyrank(set, 0, -1);
 }
 
 // Test regular suggestion generation 1
 Test(suggestion, suggestions_s1) {
-    zset_t *set = zset_new("set");
-    dict_t *d = dict_new();
+    trie_t *t = trie_new('\0');
+    match_t **set = calloc(5, sizeof(match_t*));
 
-    dict_add(d, "stall");
-    dict_add(d, "satll");
-    dict_add(d, "drall"); // success but not in list
-    dict_add(d, "malt");
-    dict_add(d, "malfkt"); // not a return
-    dict_add(d, "fn|d7~3"); // not a return
+    trie_insert_string(t, "stall");
+    trie_insert_string(t, "satll"); // success but not in list
+    trie_insert_string(t, "drall");
+    trie_insert_string(t, "malt");
+    trie_insert_string(t, "malfkt"); // not a return
+    trie_insert_string(t, "fn|d7~3"); // not a return
 
-    int rc = suggestions(set, d, "", "small", 2);
+    int rc = suggestions(set, t, "", "small", 2, 3);
     cr_assert_eq(0, rc, "suggestions() failed");
 
-    char **results = zset_revrange(set, 0, 2);
-
-    cr_assert_eq(0, strncmp(results[0], "stall", MAXLEN), 
+    cr_assert_eq(0, strncmp(set[0]->str, "stall", MAXLEN), 
                 "suggestions() first result incorrect");
-    cr_assert_eq(0, strncmp(results[1], "satll", MAXLEN), 
+    cr_assert_eq(0, strncmp(set[1]->str, "drall", MAXLEN), 
                 "suggestions() second result incorrect");
-    cr_assert_eq(0, strncmp(results[2], "malt", MAXLEN), 
+    cr_assert_eq(0, strncmp(set[2]->str, "malt", MAXLEN), 
                 "suggestions() third result incorrect");
-
-    zset_remrangebyrank(set, 0, -1);
 }
 
 /*************   These functions test the easy input functions   *************/
 
 // Test for suggestion_set_new function
 Test(suggestion, suggestion_set_new_s0) {
-    dict_t *d = dict_new();
+    trie_t *t = trie_new('\0');
 
-    dict_add(d, "only");
-    dict_add(d, "onety");
-    dict_add(d, "none");
-    dict_add(d, "fjmsdk8");
-    dict_add(d, "o=9");
+    trie_insert_string(t, "only");
+    trie_insert_string(t, "onety");
+    trie_insert_string(t, "none");
+    trie_insert_string(t, "fjmsdk8");
+    trie_insert_string(t, "o=9");
 
-    zset_t *set = suggestion_set_new(d, "one", 2);
+    match_t **set = suggestion_set_new(t, "one", 2, 4);
 
-    char **results = zset_revrange(set, 0, 3);
-
-    cr_assert_eq(0, strncmp(results[0], "none", MAXLEN), 
+    cr_assert_eq(0, strncmp(set[0]->str, "none", MAXLEN), 
                 "suggestion_set_new() first result incorrect");
-    cr_assert_eq(0, strncmp(results[1], "only", MAXLEN), 
+    cr_assert_eq(0, strncmp(set[1]->str, "o=9", MAXLEN), 
                 "suggestion_set_new() second result incorrect");
-    cr_assert_eq(0, strncmp(results[2], "onety", MAXLEN), 
+    cr_assert_eq(0, strncmp(set[2]->str, "onety", MAXLEN), 
                 "suggestion_set_new() third result incorrect");
-    cr_assert_eq(0, strncmp(results[3], "o=9", MAXLEN), 
-                "suggestion_set_new() fourth result incorrect");    
-
-    zset_remrangebyrank(set, 0, -1);
+    cr_assert_eq(0, strncmp(set[3]->str, "only", MAXLEN), 
+                "suggestion_set_new() fourth result incorrect");
 }
 
 // Test for suggestion_set_first_n function with regular input
 Test(suggestion, suggestion_set_first_n_s0) {
-    zset_t *set = zset_new("set");
+    match_t **set = calloc(5, sizeof(match_t*));
 
-    zset_add(set, "a", 1);
-    zset_add(set, "b", 2);
-    zset_add(set, "c", 3);
+    set[0] = malloc(sizeof(match_t));
+    set[1] = malloc(sizeof(match_t));
+    set[2] = malloc(sizeof(match_t));
+
+    set[0]->str = "a";
+    set[1]->str = "b";
+    set[2]->str = "c";
+
+    set[0]->edits_left = 0;
+    set[1]->edits_left = 0;
+    set[2]->edits_left = 0;
 
     char **results = suggestion_set_first_n(set, 2);
 
-    cr_assert_eq(0, strncmp(results[0], "c", MAXLEN), 
+    cr_assert_eq(0, strncmp(results[0], "a", MAXLEN), 
                 "suggestion_set_first_n() first result incorrect");
     cr_assert_eq(0, strncmp(results[1], "b", MAXLEN), 
                 "suggestion_set_first_n() second result incorrect");
-
-    zset_remrangebyrank(set, 0, -1);
 }
 
 // Test for suggestion_set_first_n function with not enough matches
 Test(suggestion, suggestion_set_first_n_f0) {
-    zset_t *set = zset_new("set");
+    match_t **set = calloc(5, sizeof(match_t*));
 
-    zset_add(set, "a", 1);
-    zset_add(set, "b", 2);
+    set[0] = malloc(sizeof(match_t));
+    set[1] = malloc(sizeof(match_t));
+    set[2] = malloc(sizeof(match_t));
+
+    set[0]->str = "a";
+    set[1]->str = "b";
+    set[2]->str = "c";
+
+    set[0]->edits_left = 0;
+    set[1]->edits_left = 0;
+    set[2]->edits_left = 0;
 
     char **results = suggestion_set_first_n(set, 3);
 
-    cr_assert_eq(0, strncmp(results[0], "b", MAXLEN), 
+    cr_assert_eq(0, strncmp(results[0], "a", MAXLEN), 
                 "suggestion_set_first_n() first result incorrect");
-    cr_assert_eq(0, strncmp(results[1], "a", MAXLEN), 
+    cr_assert_eq(0, strncmp(results[1], "b", MAXLEN), 
                 "suggestion_set_first_n() second result incorrect");
     cr_assert_null(results[2], 
                 "suggestion_set_first_n() third result incorrect");
-    
-    zset_remrangebyrank(set, 0, -1);
 }
 
 // Test for suggestion_list wrapper function simple
 Test(suggestion, suggestion_list_s0) {
-    dict_t *d = dict_new();
+    trie_t *t = trie_new('\0');
 
-    dict_add(d, "a");
+    trie_insert_string(t, "a");
 
     // lol
-    char **result = suggestion_list(d, "a", 0, 1);
+    char **result = suggestion_list(t, "a", 0, 1);
 
     cr_assert_eq(0, strncmp(result[0], "a", MAXLEN), 
                 "suggestion_list() first result incorrect");
 }
 
 Test(suggestion, suggestion_list_s1) {
-    dict_t *d = dict_new();
+    trie_t *t = trie_new('\0');
 
-    dict_add(d, "afij4-8");
-    dict_add(d, "ayij48-");
-    dict_add(d, "flij4-8");
-    dict_add(d, "afij4*8");
-    dict_add(d, "antij4-8"); 
-    dict_add(d, "nkj345yf");
-    dict_add(d, "fdjsk43");
-    dict_add(d, "s");
-    dict_add(d, "jvu4893jfDJSkds8932ujfvn.`>IW");
+    trie_insert_string(t, "afij4-8");
+    trie_insert_string(t, "ayij48-");
+    trie_insert_string(t, "flij4-8");
+    trie_insert_string(t, "afij4*8");
+    trie_insert_string(t, "antij4-8"); 
+    trie_insert_string(t, "nkj345yf");
+    trie_insert_string(t, "fdjsk43");
+    trie_insert_string(t, "s");
+    trie_insert_string(t, "jvu4893jfDJSkds8932ujfvn.`>IW");
 
 
-    char **result = suggestion_list(d, "afij4-8", 3, 3);
+    char **result = suggestion_list(t, "afij4-8", 3, 3);
 
     cr_assert_eq(0, strncmp(result[0], "afij4-8", MAXLEN), 
                 "suggestion_list() first result incorrect");
